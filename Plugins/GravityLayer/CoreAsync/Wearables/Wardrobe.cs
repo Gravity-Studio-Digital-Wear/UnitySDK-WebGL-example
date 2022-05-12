ab24390ffdbda8f4aa612336bc425b87693ace94
Jack Botvinovski <jbotvinovski@gmail.com>
2022-05-12 10:45:40 +0300

    add metaverseid to coreasync

--- Plugins/GravityLayer/CoreAsync/Wearables/Wardrobe.cs
+++ Plugins/GravityLayer/CoreAsync/Wearables/Wardrobe.cs
@@ -12,16 +12,18 @@ namespace GravityLayer.Wearables
         public List<WearableBase> Wearables { get; private set; }
 
         private IFetchWearables _connection;
+        private string _metaverseId;
 
-        public Wardrobe(IFetchWearables connection)
+        public Wardrobe(IFetchWearables connection, string metaverseId)
         {
             Wearables = new List<WearableBase>();
             _connection = connection;
+            _metaverseId = metaverseId;
         }
 
         public async Task FetchWearables()
         {
-            string jsonString = await _connection.FetchWearables();
+            string jsonString = await _connection.FetchWearablesByMetaverseId(_metaverseId);
             await FillWardrobeFromJsonString(jsonString);
         }
 
@@ -35,7 +37,7 @@ namespace GravityLayer.Wearables
                 if ((r.product.metadata != null) & (r.product.metadata.Length > 0))
                 {
                     await Downloader.DownloadImage(r.product.metadata[0].previewImage);
-                    Wearables.Add(new WearableBase(r.product.name, (Texture2D)Downloader.Texture, r.product.metadata[0].modelUrl));
+                    Wearables.Add(new WearableBase(r.product.name, (Texture2D)Downloader.Texture, r.product.metadata[0].modelUrl, _metaverseId));
                 }
             }
         }
