5fef0954db44b414c75b41e000c6939e205be84f
Jack Botvinovski <jbotvinovski@gmail.com>
2022-06-09 18:13:13 +0300

    add IWardrobe

---
+++ Wearables/IWardrobe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GravityLayer.Wearables
+{
+    public interface IWardrobe
+    {
+        public List<IWearable> Wearables { get; }
+
+        public Task FetchWearables();
+    }
+}
