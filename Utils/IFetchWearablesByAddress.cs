ff86229da73376437a12a40f6a16eece52f547db
Jack Botvinovski <jbotvinovski@gmail.com>
2022-06-09 18:51:35 +0300

    add new api support

---
+++ Utils/IFetchWearablesByAddress.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace GravityLayer.Utils
+{
+    public interface IFetchWearablesByAddress
+    {
+        public Task<string> GetNFTsByAddress(string address);
+    }
+}
