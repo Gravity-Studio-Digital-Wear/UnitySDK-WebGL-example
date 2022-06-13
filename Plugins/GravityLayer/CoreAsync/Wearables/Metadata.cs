c594bb1ab87ecbaba50576b03971f7db5d1add71
Jack Botvinovski <jbotvinovski@gmail.com>
2022-06-13 17:05:43 +0300

    add Metadata class

---
+++ Plugins/GravityLayer/CoreAsync/Wearables/Metadata.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GravityLayer.Wearables
+{
+    public class Metadata
+    {
+        public Texture2D PreviewImage { get; private set; }
+        public string ModelUrl { get; private set; }
+
+        public Dictionary<string, string> Attributes;
+
+        public Metadata(Texture2D previewImage, string modelUrl, Dictionary<string,string> attributes = null)
+        {
+            PreviewImage = previewImage;
+            ModelUrl = modelUrl;
+            Attributes = attributes;
+        }
+    }
+}
