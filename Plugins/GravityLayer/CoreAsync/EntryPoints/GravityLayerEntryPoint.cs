779eb560917656a0467a51cf30a072ace4e82b65
Jack Botvinovski <jbotvinovski@gmail.com>
2022-06-13 17:16:52 +0300

    add description

--- Plugins/GravityLayer/CoreAsync/EntryPoints/GravityLayerEntryPoint.cs
+++ Plugins/GravityLayer/CoreAsync/EntryPoints/GravityLayerEntryPoint.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 
 namespace GravityLayer
 {
+    /// <summary>
+    /// The GravityLayerEntryPoint class contains all public points to deal with Gravity Layer API. It serves as an example.
+    /// </summary>
+    /// <remarks>
+    /// Authentication is done by receiving a JWT token for a User via signing a message from Gravity Layer API. See details in documentation.
+    /// </remarks>
     public class GravityLayerEntryPoint
     {
         public Connection GLayerConnection { get; private set; }
